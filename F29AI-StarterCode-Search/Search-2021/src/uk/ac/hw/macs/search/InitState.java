1acf149533712ff538263ecbb5c839d0d253ae49
Áron Széles <szelesaron39@gmail.com>
2021-10-10 10:44:02 +0100

    added files

---
+++ F29AI-StarterCode-Search/Search-2021/src/uk/ac/hw/macs/search/InitState.java
@@ -0,0 +1,32 @@
+package uk.ac.hw.macs.search;
+
+public class InitState implements State{
+	
+	private int heuristic;
+	private boolean goal;
+
+	
+	//two constructors
+	public InitState(int heuristic)
+	{
+		this.heuristic = heuristic;
+		this.goal = false;
+	}
+	
+	public InitState (int heuristic, boolean goal) {
+		this.heuristic = heuristic;
+		this.goal = goal;
+	}
+	
+	
+	@Override
+	public boolean isGoal() {
+		return this.goal;
+	}
+
+	@Override
+	public int getHeuristic() {
+		return this.heuristic;
+	}
+
+}
